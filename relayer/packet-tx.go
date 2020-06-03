e33c61f6356eedc7fde2adeca67008e605efeed2
Marcel Pohland <m.pohland@mwaysolutions.com>
2020-06-03 13:59:50 +0200

    Revert "fraudulent transfer token"
    This reverts commit a832d8cc23bc3cd4871965d8f78cb3ab6fb50c50.

--- relayer/packet-tx.go
+++ relayer/packet-tx.go
@@ -110,9 +110,7 @@ func (src *Chain) SendTransferBothSides(dst *Chain, amount sdk.Coin, dstAddr sdk
 
 	// reconstructing packet data here instead of retrieving from an indexed node
 	xferPacket := src.PathEnd.XferPacket(
-		sdk.NewCoins(sdk.Coin{
-			Denom:  amount.Denom,
-			Amount: sdk.NewInt(0)}),
+		sdk.NewCoins(amount),
 		srcAddrString,
 		dstAddrString,
 	)
